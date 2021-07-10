acd6b0eac76c6283c52868d574cfea09b30b43c3
t_takano <cerberus1974@gmail.com>
2021-07-10 22:37:22 +0900

    mention移行の入力がspan内に入ってしまうので半角スペースを末尾に追加

--- basic_html/mention.js
+++ basic_html/mention.js
@@ -3,7 +3,20 @@ const editor = document.querySelector('#text_area');
 
 selector.onchange = () => {
     const op = selector.options[selector.selectedIndex];
-    
+
     // 面倒なのでつねに末尾に挿入する
-    editor.innerHTML = `${editor.innerHTML}<span id="${op.value}" class="mention">${op.text}</span>`;
+    editor.innerHTML = `${editor.innerHTML}<span id="${op.value}" class="mention">${op.text}</span>&nbsp;`;
+
+    // キャレットをテキストエリアの末尾に移動
+    editor.focus();
+    moveCaret(editor);
+}
+
+const moveCaret = (editor) => {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.setStart(editor.lastChild, 1);
+    range.collapse(true);
+    selection.removeAllRanges();
+    selection.addRange(range);
 }
