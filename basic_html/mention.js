3b4ac3bf60c4ce2145e12e98edc03dad009d4a4c
t_takano <cerberus1974@gmail.com>
2021-07-10 21:56:40 +0900

    mentionにspanタグを付ける

--- basic_html/mention.js
+++ basic_html/mention.js
@@ -2,8 +2,8 @@ const selector = document.querySelector('#user_list');
 const editor = document.querySelector('#text_area');
 
 selector.onchange = () => {
-    const name = selector.options[selector.selectedIndex].text;
+    const op = selector.options[selector.selectedIndex];
     
     // 面倒なのでつねに末尾に挿入する
-    editor.innerHTML = editor.innerHTML + name;
+    editor.innerHTML = `${editor.innerHTML}<span id="${op.value}" class="mention">${op.text}</span>`;
 }
